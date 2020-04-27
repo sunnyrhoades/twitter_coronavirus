75d5f27dc65fc2c413949360212f93ea8e5fdf05
Sunny Rhoades <smrb2016@mymail.pomona.edu>
2020-04-27 06:46:02 -0700

    added country

--- src/map.py
+++ src/map.py
@@ -37,6 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
@@ -59,9 +60,16 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # search hashtags
                 for hashtag in hashtags:
+                    try:
+                        country = tweet['place']['country_code']
+                    except TypeError:
+                        country = 'und'
+                        pass
                     lang = tweet['lang']
                     if hashtag in text:
+                        counter_country[hashtag][country] += 1
                         counter_lang[hashtag][lang] += 1
+                    counter_country['_all'][country] += 1
                     counter_lang['_all'][lang] += 1
 
 # open the outputfile
@@ -76,3 +84,7 @@ print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
 
+output_path_country = output_path_base+'.country'
+print('saving', output_path_country)
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
